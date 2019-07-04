0997c9743222c39a52d461b3b1777ffebd43ad83
Josselin Auguste <jauguste@iblop.net>
2019-07-04 21:23:10 +0200

    use default python

--- lizshe.plugin.zsh
+++ lizshe.plugin.zsh
@@ -1,4 +1,4 @@
 prompt_lizshe() {
-  HOPE=$(python2 -c "import datetime; h=80*365.25; b=datetime.datetime.strptime('$POWERLEVEL9K_LIZSHE_BIRTH', '%Y-%m-%d'); e=datetime.datetime.now()-b; print(int(e.days/h*100))")
+  HOPE=$(python -c "import datetime; h=80*365.25; b=datetime.datetime.strptime('$POWERLEVEL9K_LIZSHE_BIRTH', '%Y-%m-%d'); e=datetime.datetime.now()-b; print(int(e.days/h*100))")
   "$1_prompt_segment" "$0" "$2" "darkgreen" "yellow4" "$HOPE %%" "LIZSHE_HEART"
 }
