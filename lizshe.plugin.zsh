b12ae216944bd6127a1db6bb22a5d56cb1497fcc
Josselin Auguste <jauguste@iblop.net>
2019-08-12 18:16:24 +0200

    fix colors

--- lizshe.plugin.zsh
+++ lizshe.plugin.zsh
@@ -1,4 +1,4 @@
 prompt_lizshe() {
   HOPE=$(python -c "import datetime; h=80*365.25; b=datetime.datetime.strptime('$POWERLEVEL9K_LIZSHE_BIRTH', '%Y-%m-%d'); e=datetime.datetime.now()-b; print(int(e.days/h*100))")
-  "$1_prompt_segment" "$0" "$2" "darkgreen" "yellow4" "$HOPE %%" "LIZSHE_HEART"
+  "$1_prompt_segment" "$0" "$2" "green" "black" "$HOPE %%" "LIZSHE_HEART"
 }
